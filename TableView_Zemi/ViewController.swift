8715302dd5232af4ed569b2e64ddabb58b2aaf9d
oRiku <chestnutman7415@gmail.com>
2020-08-26 19:29:00 +0900

    Initial Commit

---
+++ TableView_Zemi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView_Zemi
+//
+//  Created by yanagimachi_riku on 2020/08/26.
+//  Copyright © 2020 Riku_Yanagimachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
